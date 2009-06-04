4274d055173cb08bcdab06915998e0d902c42eaa
highfive <highfive@aa33edd8-d469-0410-94bb-77eb1adef642>
2009-06-04 15:25:26 +0000

    git-svn-id: http://svn2.xp-dev.com/svn/highfive-Team5@41 aa33edd8-d469-0410-94bb-77eb1adef642

--- SongScheduler/src/model/SongScheduler.java
+++ SongScheduler/src/model/SongScheduler.java
@@ -18,6 +18,17 @@ public class SongScheduler {
         
     }
 
+    /**
+     * generateOneHour
+     *
+     * Assuming no schedule exists at the given time, generates a one hour schedule
+     * of eligible random songs.
+     * Returns the generated Schedule
+     *
+     * @author liufeng & aprilbugnot
+     * @param startTime
+     * @return Schedule
+     */
     public Schedule generateOneHour(Time startTime) {
         Schedule result = new Schedule(startTime);
 
@@ -61,10 +72,24 @@ public class SongScheduler {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        // TODO: april&feng - should this be where we add this schedule to tentativeSchedules?
         return result;
     }
 
+    /**
+     * canAddThisSong
+     *
+     * Checks schedules before and after the startTime -- among the tentative schedules,
+     * as well as the database -- to see whether they already contain the given song.
+     * Returns true if we are free to add the song (i.e. the preceeding and proceeding
+     * schedules do not contain the song), or false if we cannot add the song.
+     *
+     * @author liufeng & aprilbugnot
+     * @param song
+     * @param startTime
+     * @param connection
+     * @return boolean
+     */
     private boolean canAddThisSong(Song song, Time startTime, Connection connection) {
         Time previousHour = startTime.getPreviousHour();
         Time nextHour = startTime.getNextHour();
@@ -82,6 +107,29 @@ public class SongScheduler {
             result &= tentativeSchedule[nextHour.getDay()][nextHour.getHour()].contains(song);
         }
 
+        return result;
+    }
+
+    /**
+     * getSchedule
+     *
+     * If a tentative schedule exists for the specified time -- first check tentative schedule,
+     * then check database -- return this Schedule object;
+     * If no schedule exists for the specified time, return null.
+     *
+     * @author aprilbugnot
+     * @param startTime
+     * @return Schedule of the specified time, or null if no schedule exists
+     */
+    public Schedule getSchedule(Time startTime) {
+        Schedule result = null;
+
+        if (tentativeSchedule[startTime.getDay()][startTime.getHour()] == null) {
+            // check db;
+        } else {
+            result = tentativeSchedule[startTime.getDay()][startTime.getHour()];
+        }
+        
         return result;
     }
 }
