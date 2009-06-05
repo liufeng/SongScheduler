25043a28132e5ebdc352a524ffb8d370967c1379
highfive <highfive@aa33edd8-d469-0410-94bb-77eb1adef642>
2009-06-05 20:42:55 +0000

    Added a function that gets all of the songs from the database ordered by popularity
    git-svn-id: http://svn2.xp-dev.com/svn/highfive-Team5@68 aa33edd8-d469-0410-94bb-77eb1adef642

--- SongScheduler/src/model/SongDBI.java
+++ SongScheduler/src/model/SongDBI.java
@@ -13,6 +13,44 @@ import java.util.ArrayList;
  */
 public class SongDBI {
 
+    public static ArrayList getSongsByPopularity(){
+        ArrayList theSongs = new ArrayList();
+        Song currSong;
+        String sqlStatement = "SELECT * FROM song ORDER BY popularity, title;";
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:song.db");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(sqlStatement);
+
+            while (rs.next()) {
+                String title = rs.getString("title");
+                String performer = rs.getString("performer");
+                String recordingTitle = rs.getString("recordingTitle");
+                String recordingType = rs.getString("recordingType");
+                String year = rs.getString("year");
+                int    length = rs.getInt("length");
+                int    accessNumber = rs.getInt("accessNumber");
+                int    popularity = rs.getInt("popularity");
+                int    playCount = rs.getInt("playCount");
+                Time   addedTime = new Time(rs.getString("addedTime"));
+                Time   lastPlayed = new Time(rs.getString("lastPlayed"));
+                double priority = rs.getDouble("priority");
+
+                currSong = new Song(title, performer, recordingTitle, recordingType, year, length, accessNumber, popularity, playCount, addedTime, lastPlayed, priority);
+                theSongs.add(currSong);
+            }
+            rs.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return theSongs;
+    }
+
     public static ArrayList getSongs( String songName ){
         ArrayList theSongs = new ArrayList();
         Song currSong;
