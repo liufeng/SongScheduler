59ef74d46dfe3181f618c66c1507e0f32dee7997
highfive <highfive@aa33edd8-d469-0410-94bb-77eb1adef642>
2009-06-09 01:36:07 +0000

    Added database creation if it doesn't exist.
    - Kurtis
    
    git-svn-id: http://svn2.xp-dev.com/svn/highfive-Team5@112 aa33edd8-d469-0410-94bb-77eb1adef642

--- SongScheduler/src/songscheduler/Main.java
+++ SongScheduler/src/songscheduler/Main.java
@@ -6,6 +6,8 @@
 package songscheduler;
 
 import gui.*;
+import model.Database;
+import java.io.File;
 
 public class Main {
 
@@ -13,6 +15,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main ( String args[] ) {
+        // Check for the database
+        File f = new File("song.db");
+        if ( !f.exists() )
+        {
+            Database.init();
+            Database.updateSongInfo();
+        }
+        // TODO: Update every 7 days instead of only first time.
+
+        // Run the program
         java.awt.EventQueue.invokeLater( new Runnable() {
             public void run () {
                 new SongSchedulerWindow().setVisible( true );
