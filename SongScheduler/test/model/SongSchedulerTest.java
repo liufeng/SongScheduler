7e37e37dde78d639bf70f32dca059fa0a67477e5
highfive <highfive@aa33edd8-d469-0410-94bb-77eb1adef642>
2009-06-25 18:44:40 +0000

    Add some unit tests for testing generateMultipleHours() and generateDays(), but they don't work properly since the way we generate multiple-hour schedule.
    -Feng
    
    git-svn-id: http://svn2.xp-dev.com/svn/highfive-Team5@119 aa33edd8-d469-0410-94bb-77eb1adef642

--- SongScheduler/test/model/SongSchedulerTest.java
+++ SongScheduler/test/model/SongSchedulerTest.java
@@ -41,7 +41,6 @@ public class SongSchedulerTest {
     /**
      * Test of generateOneHour method, of class SongScheduler.
      */
-    @Ignore
     @Test
     public void testGenerateOneHour() {
         System.out.println("generateOneHour");
@@ -56,4 +55,52 @@ public class SongSchedulerTest {
         assertTrue(result.getDuration() <= 2880000);
     }
 
+    /**
+     * Test of generateMultipleHours method, of class SongScheduler.
+     *
+     * still has problem since we generate one-hour-schedules not a whole one.
+     */
+    @Test
+    @Ignore
+    public void testGenerateMultipleHours() {
+        System.out.println("generateMultipleHours");
+        Time startTime = new Time();
+        SongScheduler instance = new SongScheduler(startTime);
+        Schedule result;
+
+        for (int hours = 0; hours < 24; hours++) {
+            instance.generateMultipleHours(startTime, hours);
+            result = instance.getSchedule(startTime);
+
+            int minLength = hours * 43 * 60 * 1000;
+            int maxLength = hours * 48 * 60 * 1000;
+            System.out.println(result.getDuration());
+            assertTrue(result.getDuration() >= minLength);
+            assertTrue(result.getDuration() <= maxLength);
+        }
+    }
+    
+    /**
+     * Test of generateDays method, of class SongScheduler.
+     *
+     * still has problem since we generate one-hour-schedules not a whole one.
+     */
+    @Test
+    @Ignore
+    public void testGenerateDays() {
+        System.out.println("generateDays");
+        Time startTime = new Time(2009, 6, 20, 0, 0, 0);
+        SongScheduler instance = new SongScheduler(startTime);
+        Schedule result;
+
+        for (int days = 0; days < 7; days++) {
+            instance.generateDays(startTime, days);
+            result = instance.getSchedule(startTime);
+
+            int minLength = days * 24 * 43 * 60 * 1000;
+            int maxLength = days * 24 * 48 * 60 * 1000;
+            assertTrue(result.getDuration() >= minLength);
+            assertTrue(result.getDuration() <= maxLength);
+        }
+    }
 }
