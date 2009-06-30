d5f949fba51a833cf3809ed985a28d7ede7a7a24
highfive <highfive@aa33edd8-d469-0410-94bb-77eb1adef642>
2009-06-30 16:12:03 +0000

    A class to authenticat the admin user.
    -Feng
    
    git-svn-id: http://svn2.xp-dev.com/svn/highfive-Team5@126 aa33edd8-d469-0410-94bb-77eb1adef642

---
+++ SongScheduler/src/model/Authentication.java
@@ -0,0 +1,91 @@
+/*
+ * For authentication the password for the admin user.
+ */
+
+package model;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author liufeng
+ */
+public abstract class Authentication {
+    private boolean isAuthenticated;
+    private String password;
+    private static final String PASS_FILE = "passwd";
+
+    /**
+     * Initialize the password and the user status.
+     */
+    public void init() {
+        isAuthenticated = false;
+        password = "";
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(PASS_FILE));
+            password = in.readLine();
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check if the user is authenticationed.
+     */
+    public boolean isAuthenticated() {
+        return isAuthenticated;
+    }
+
+    /**
+     * Check if the password is correct. And change the user status.
+     * @param word The user input password
+     * @return <strong>true</strong> if the password is correct;
+     *          <strong>false</strong> otherwise.
+     */
+    public boolean checkPassword(String word) {
+        if (password.equals(encrypt(word))) {
+            isAuthenticated = true;
+        } else {
+            isAuthenticated = false;
+        }
+
+        return isAuthenticated;
+    }
+
+    /**
+     * Modify the admin password.
+     * @param newPass the new password.
+     * @return <strong>true</strong> if the modification success;
+     *          <strong>false</strong> otherwise (the user is not authenticated)
+     */
+    public boolean modifyPassword(String newPass) {
+        if (isAuthenticated()) {
+            String encryptedPass = encrypt(newPass);
+            try {
+                BufferedWriter out = new BufferedWriter(new FileWriter(PASS_FILE));
+                out.write(encryptedPass);
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * To encrypt the string. May use MD5 algo. Haven't implement yet.
+     * @param pass
+     * @return the encrypted password.
+     */
+    private String encrypt(String pass) {
+        return pass;
+    }
+}
